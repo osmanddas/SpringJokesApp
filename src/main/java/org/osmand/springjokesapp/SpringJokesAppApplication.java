faa6e30f99c8fa90c3f80f20da19ab43fb494318
Osmand Das <osmandpdas@gmail.com>
2018-05-16 19:48:56 -0400

    Updated files

--- src/main/java/org/osmand/springjokesapp/SpringJokesAppApplication.java
+++ src/main/java/org/osmand/springjokesapp/SpringJokesAppApplication.java
@@ -2,8 +2,10 @@ package org.osmand.springjokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:jokes-config.xml")
 public class SpringJokesAppApplication {
 
 	public static void main(String[] args) {
