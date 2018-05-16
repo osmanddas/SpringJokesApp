d851bfb5859a2b5da67bef44430e4797a9ebc397
Osmand Das <osmandpdas@gmail.com>
2018-05-16 16:03:04 -0400

    Updated files

--- src/main/java/org/osmand/springjokesapp/controller/JokesController.java
+++ src/main/java/org/osmand/springjokesapp/controller/JokesController.java
@@ -1,11 +1,27 @@
 package org.osmand.springjokesapp.controller;
 
+import org.osmand.springjokesapp.service.JokesService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JokesController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private JokesService jokesService;
+
+    public JokesController(JokesService jokesService) {
+        this.jokesService = jokesService;
+    }
+
+    @RequestMapping({"/"})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokesService.getJokes());
+        return "chucknorris";
+    }
+
+
 }
